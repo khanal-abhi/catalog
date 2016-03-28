c8b416db776feac8416f0b43e782118d39d0e5fa
Abhi Khanal <khanal.abhi@gmail.com>
2016-03-28 16:28:43 -0400

    New category is good.

--- catalog.py
+++ catalog.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
 
 from database_setup import Base, Item, Category, db_url
@@ -18,11 +18,13 @@ app = Flask(__name__, static_url_path='/static')
 def load_css():
     return app.send_static_file('style.css')
 
+
 @app.route('/')
 def index():
     categories = session.query(Category).all()
+    items = session.query(Item).order_by(desc(Item.id)).limit(20)
 
-    return render_template('home.html', categories=categories)
+    return render_template('home.html', categories=categories, items=items)
 
 
 @app.route('/category/<int:category_id>/')
@@ -35,6 +37,24 @@ def show_category(category_id):
 
     except:
         return "404"
+
+
+@app.route('/category/new/', methods=['POST', 'GET'])
+def new_category():
+    if request.method == 'GET':
+        return render_template('new_category.html')
+
+    if request.method == 'POST':
+        name = request.form['name']
+        new_category = Category(name=name)
+        session.add(new_category)
+        session.commit()
+        return redirect(url_for('index'), code=301)
+
+
+@app.route('/item/new/')
+def new_item():
+    return render_template('new_item.html')
 
 
 if __name__ == '__main__':
