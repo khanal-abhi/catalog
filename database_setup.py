f1cd9152a2dca18c27dd6891b198cbc3b3eeb2d1
Abhi Khanal <khanal.abhi@gmail.com>
2016-03-28 13:17:18 -0400

    cleaned up the basic server.

---
+++ database_setup.py
@@ -0,0 +1,40 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Category(Base):
+    __tablename__ = 'category'
+    id = Column(
+        Integer, primary_key=True
+    )
+    name = Column(
+        String(80), nullable=False
+    )
+
+
+class Item(Base):
+    __tablename__ = 'item'
+    id = Column(
+        Integer, primary_key=True
+    )
+    title = Column(
+        String(80), nullable=False
+    )
+    description = Column(
+        String, nullable=False
+    )
+    category_id = Column(
+        Integer, ForeignKey('category.id')
+    )
+    category = relationship(Category)
+
+
+
+
+engine = create_engine("postgresql:///catalog")
+Base.metadata.create_all(engine)
