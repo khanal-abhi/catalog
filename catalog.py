17a1818ede235746cb075b0ec8e37b8ea71e00eb
Abhi Khanal <khanal.abhi@gmail.com>
2016-03-30 09:35:01 -0400

    Added google plus login.

--- catalog.py
+++ catalog.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template, url_for, request, redirect, flash, \
-    jsonify
+    jsonify, make_response
 
 from sqlalchemy import create_engine, desc, func
 from sqlalchemy.orm import sessionmaker
 
 from database_setup import Base, Item, Category, db_url
 from flask import session as login_session
-import random, string
+import random
+import string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+
+import requests
+
+CLIENT_ID = json.loads(open('client_secrets.json', 'r').read())['web'][
+    'client_id']
+
+
 
 engine = create_engine(db_url)
 Base.metadata.bind = engine
@@ -311,6 +323,75 @@ def login():
     login_session['csrf_token'] = csrf_token
     return render_template('login.html', csrf_token=csrf_token)
 
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    if request.args.get('csrf_token') != login_session['csrf_token']:
+        response = make_response(json.dumps('Invalid csrf_token', 401))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    code = request.data
+    try:
+        oauth_flow = flow_from_clientsecrets('client_secrest.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+
+    except FlowExchangeError:
+        response = make_response(json.dumps('Failed to upgrade the '
+                                            'authorization code', 401))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    http = httplib2.Http()
+    result = json.loads(http.request(url, 'GET')[1])
+
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error'), 500))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(json.dumps(
+            'Token\'s user id does not match given user\'s id.', 401))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(json.dumps(
+            'Token\'s client id does not match app\'s.', 401))
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_credentials = login_session['credentials']
+    stored_gplus_id = login_session['gplus_id']
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('User is already connected.', 200))
+        response.headers['Content-Type'] = 'applcation/json'
+
+    login_session['credentials'] = credentials
+    login_session['gplus_id'] = gplus_id
+
+    userinfo_url = 'https://www.googleapis.com/oauth2/v1/userinfo'
+    params = {
+        'access_token': credentials.access_token,
+        'alt': 'json'
+    }
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    csrf_token = ''.join(random.choice(string.uppercase + string.digits) for
+                         x in xrange(32))
+    login_session['csrf_token'] = csrf_token
+    flash('You have been logged in', 'success')
+    return render_template('login.html', csrf_token=csrf_token)
 
 if __name__ == '__main__':
     app.debug = True
